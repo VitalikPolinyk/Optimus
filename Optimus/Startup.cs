062a4fac73d8214abcba74efa715b7bb681c5c90
VitalikPolinyk <vitaliy21039.96@mail.ru>
2017-09-20 11:34:22 +0300

    Add project files.

---
+++ Optimus/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Optimus.Startup))]
+namespace Optimus
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
